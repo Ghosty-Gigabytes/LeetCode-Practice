f206ced4fd8dfa522ff1f5651fe1940594891a3d
Yatharth Shahi <57322205+Ghosty-Gigabytes@users.noreply.github.com>
2025-05-30 12:32:04 +0530

    Time: 11 ms (6.93%) | Memory: 42.3 MB (37.57%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,26 +2,19 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> list = new ArrayList<>();
         if (digits.length() != 0) {
-            Integer n = Integer.parseInt(digits);
-            int d=0;
-            while(n>0){
-                d = d*10 + n%10;
-                n /=10;
-            }
-            Map<Integer, String> map = Map.of(
-                    2, "abc",
-                    3, "def",
-                    4, "ghi",
-                    5, "jkl",
-                    6, "mno",
-                    7, "pqrs",
-                    8, "tuv",
-                    9, "wxyz");
+            Map<String, String> map = Map.of(
+                    "2", "abc",
+                    "3", "def",
+                    "4", "ghi",
+                    "5", "jkl",
+                    "6", "mno",
+                    "7", "pqrs",
+                    "8", "tuv",
+                    "9", "wxyz");
             String val;
             int i = 0;
-            while (d > 0) {
-                val = map.get(d % 10);
-                d /= 10;
+            while (i < digits.length()) {
+                val = map.get(""+digits.charAt(i));
                 if (i == 0) {
                     for (int j = 0; j < val.length(); j++) {
                         list.add(String.valueOf(val.charAt(j)));
@@ -34,6 +27,7 @@ class Solution {
                         it.remove();
                         while (j < val.length()) {
                             String temp = "" + s + val.charAt(j);
+                            System.out.println(temp);
                             it.add(temp);
                             j++;
                         }
