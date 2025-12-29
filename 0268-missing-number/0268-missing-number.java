63c20633b8d9ad5c1da535f66e17074b7c49dbaa
Yatharth Shahi <yatharth_vats@outlook.com>
2025-12-29 20:15:39 +0530

    0268-MISSING-NUMBER Stats: Time: 0 ms (100%), Space: 47.4 MB (44.62%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int currentSum = 0;
-        int actualSum = 0;
-        int counter = nums.length;
+        int actualSum = (nums.length*(nums.length +1))/2;
         for (int i = 0; i < nums.length; i++){
             currentSum += nums[i];
-            actualSum += counter;
-            counter--;
         }
         return actualSum - currentSum;
     }
